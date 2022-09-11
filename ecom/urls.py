f9d97dae538a4aaa0fa30abcb0e7b65320678543
maheh <maheshmb7712@gmail.com>
2022-09-11 11:05:27 +0530

    added app

--- ecom/urls.py
+++ ecom/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls')),
+    path('api/',include('app.urls'))
 ]
