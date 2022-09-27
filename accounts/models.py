cd5dff84f2de24322ee2e22465c4d68ddb3e463f
maheh <maheshmb7712@gmail.com>
2022-09-27 23:23:39 +0530

    started creating custom user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+from statistics import mode
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
 # Create your models here.
+
+class Account(AbstractBaseUser):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    username=models.CharField(max_length=50,unique=True)
+    email=models.EmailField(max_length=50,unique=True)
+    phone_number=models.CharField(max_length=50)
