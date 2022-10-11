f38fdacf05b1af898a5e36e4c01ec5c2ba1ccd73
maheh <maheshmb7712@gmail.com>
2022-10-11 19:54:18 +0530

    created new branch named master and created custom user

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,44 @@
 from email.policy import default
+from importlib.util import find_spec
+from re import T
 from statistics import mode
 from tkinter.tix import Tree
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self,first_name,last_name,username,email,password=None):
+        if not email:
+            raise ValueError('user must have an email address')
+        if not username:
+            raise ValueError('user must have an username')
+        user=self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,first_name,last_name,username,email,password):
+        user=self.model(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,  
+            first_name=first_name,
+            last_name=last_name,
+
+        )
+        user.is_admin=True
+        user.is_active=True
+        user.is_staff=True
+        user.is_superadmin=True
+        user.save(using=self._db)
+        return user
 
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
@@ -21,6 +56,8 @@ class Account(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects=MyAccountManager()
 
     def __str__(self):
         return self.email
