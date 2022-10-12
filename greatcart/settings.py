169568e3ee323b49d4accaa349f5a6ae6245243b
maheh <maheshmb7712@gmail.com>
2022-10-12 18:24:12 +0530

    formatting

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'greatcart.wsgi.application'
 
-AUTH_USER_MODEL='accounts.Account'
+AUTH_USER_MODEL = 'accounts.Account'
 
 
 # Database
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR /'static'
-STATICFILES_DIRS=[
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
     'greatcart/static'
 ]
