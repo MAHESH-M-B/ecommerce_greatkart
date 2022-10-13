417e4b1d0691abf02351413ce13e783018a59c0f
maheh <maheshmb7712@gmail.com>
2022-10-13 21:37:41 +0530

    nothing

--- category/admin.py
+++ category/admin.py
@@ -5,4 +5,8 @@ from .models import Category
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields={'slug': ('category_name',)}
     list_display=('category_name','slug')
+
+
+
+    
 admin.site.register(Category,CategoryAdmin)
