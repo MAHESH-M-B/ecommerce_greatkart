ba4461f41576cdca93b7c3b95cb7a02d13c9e337
maheh <maheshmb7712@gmail.com>
2022-10-15 21:44:03 +0530

    created new branch for serializers

---
+++ category/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import Categoryviewset
+
+router = routers.DefaultRouter()
+router.register(r'Category', Categoryviewset)
+
+urlpatterns = [
+   path('', include(router.urls)),
+]
