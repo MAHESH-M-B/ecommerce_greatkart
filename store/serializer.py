7e303b0fe5bae9c1134bf7627418d1d5defd28ed
maheh <maheshmb7712@gmail.com>
2022-10-16 15:50:36 +0530

    created serializer for store,accounts,category

--- store/serializer.py
+++ store/serializer.py
@@ -6,4 +6,5 @@ from .models import Products
 class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model=Products
-        fields=('product_name','slug','description','price','images','stock','is_available','category','created_date','modified_date')
+        # fields=('product_name','slug','description','price','images','stock','is_available','category','created_date','modified_date')
+        fields='__all__'
