feb036d3e6ce63b899fc6dc306bb3a57dc0c507e
maheh <maheshmb7712@gmail.com>
2022-10-24 11:03:01 +0530

    nothing

--- store/urls.py
+++ store/urls.py
@@ -1,17 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
-
 from store import views
 from .views import Productviewset
 
 router = routers.DefaultRouter()
 router.register(r'Product', Productviewset)
-
-
 urlpatterns = [
     path('store/', views.store, name='store'),
     path('api/product_api/', include(router.urls)),
-    path('store/<slug:category_slug>/',views.store, name='product_by_category'),   
+    path('store/<slug:category_slug>/', views.store, name='product_by_category'),
 ]
-
-
