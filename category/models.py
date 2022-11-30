ce384dbcbe0f04678c4cdde14d7ee67f8a467b23
maheh <maheshmb7712@gmail.com>
2022-11-30 21:48:26 +0530

    nothing

--- category/models.py
+++ category/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
-
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
