03a55bf50cee1e8fdef15db06c9c22ba6b341d22
maheh <maheshmb7712@gmail.com>
2022-12-07 22:23:07 +0530

    created single product in url wise

--- store/models.py
+++ store/models.py
@@ -15,5 +15,8 @@ class Product(models.Model):
     created_date = models.DateField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
+
+    def get_url(self):
+        return reverse('product_in_detail',args=[self.category.slug,self.slug])
     def __str__(self):
         return self.product_name
--- store/views.py
+++ store/views.py
@@ -28,11 +28,13 @@ def store(request, category_slug=None):
 def product_detail(request,category_slug,product_slug):
     try:
         single_product=Product.objects.get(category__slug=category_slug,slug=product_slug)
+        print(single_product)
     except Exception:
         raise Exception
     context={
-        'single_product':single_product
+        'single_product':single_product,
     }
+    print(context)
     return render(request,'store/product_detail.html',context)
 
 class Productviewset(viewsets.ModelViewSet):
